3c6544965fe4a47486e379f7bd3e84b0be571baf
hyunseo <hforestna@gmail.com>
2025-04-13 12:00:53 +0900

    get session info

--- src/main/java/com/example/springjwt/controller/MainController.java
+++ src/main/java/com/example/springjwt/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.example.springjwt.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * fileName     : MainController.java
@@ -17,6 +22,13 @@ public class MainController {
 
     @GetMapping
     public String mainP() {
-        return "Main Controller";
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority grantedAuthority = iterator.next();
+        String role = grantedAuthority.getAuthority();
+
+        return "Main Controller" + username + " " + role;
     }
 }
